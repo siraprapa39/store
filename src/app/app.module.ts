3a48653596d55602a45a8ca5181e668336503c8f
Train015 <Train015@MOJ.GO.TH>
2020-01-14 14:31:46 +0700

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import {HttpClientModule} from '@angular/common/http';
+import {ReactiveFormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,6 +10,9 @@ import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { CartComponent } from './cart/cart.component';
+import { from } from 'rxjs';
+import { ShippingComponent } from './shipping/shipping.component';
 
 @NgModule({
   declarations: [
@@ -15,14 +20,20 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
     TopBarComponent,
     ProductListComponent,
     ProductAlertsComponent,
-    ProductDetailsComponent
+    ProductDetailsComponent,
+    CartComponent,
+    ShippingComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
-      {path:'products/:productId',component : ProductDetailsComponent}
+      {path:'products/:productId',component : ProductDetailsComponent},
+      {path:'cart',component : CartComponent},
+      {path:'shipping',component : ShippingComponent}
     ])
   ],
   providers: [],
--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -6,7 +7,10 @@ import { Injectable } from '@angular/core';
 export class CartService {
 
   items = [];
-  constructor() { }
+  constructor(
+    private http : HttpClient
+  ) {}
+
  addToCart(product){
    this.items.push(product);
  }
@@ -18,5 +22,9 @@ export class CartService {
  clearCart(){
    this.items = [];
    return this.items;
+ }
+
+ getShippingPrices(){
+   return this.http.get('assets/shipping.json');
  }
 }
